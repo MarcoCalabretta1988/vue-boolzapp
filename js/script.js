cc3aff6f6f2dc686fbf2d3f5a8fa22b8b40a4820
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-01-13 12:10:18 +0100

    Add delet function whit id problem

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ const app = Vue.createApp({
 
             contacts: [
                  {
+                   id: 1,
                    name: 'Michele Lorem',
                    avatar: '_1',
                    visible: true,
@@ -21,75 +22,89 @@ const app = Vue.createApp({
                     {
                      date: '10/01/2020 15:30:55',
                      text: 'Hai portato a spasso il cane?',
-                     status: 'sent'
+                     status: 'sent',
+                     isBoxClick: false,
                    },
                    {
                      date: '10/01/2020 15:50:00',
                      text: 'Ricordati di dargli da mangiare',
-                     status: 'sent'
+                     status: 'sent',
+                     isBoxClick: false,
                    },
                    {
                      date: '10/01/2020 16:15:22',
                      text: 'Tutto fatto!',
-                     status: 'received'
+                     status: 'received',
+                     isBoxClick: false,
                    }
                    ],
                  },
                  {
+                   id: 2,
                    name: 'Fabio Ipsum',
                    avatar: '_2',
                    visible: true,
                    messages: [{
                      date: '20/03/2020 16:30:00',
                      text: 'Ciao come stai?',
-                     status: 'sent'
+                     status: 'sent',
+                     isBoxClick: false,
                    },
                    {
                      date: '20/03/2020 16:30:55',
                      text: 'Bene grazie! Stasera ci vediamo?',
-                     status: 'received'
+                     status: 'received',
+                     isBoxClick: false,
                    },
                    {
                      date: '20/03/2020 16:35:00',
                      text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                     status: 'sent'
+                     status: 'sent',
+                     isBoxClick: false,
                    }
                    ],
                  },
                  {
+                   id: 3,
                    name: 'Samuele Volano',
                    avatar: '_3',
                    visible: true,
                    messages: [{
                      date: '28/03/2020 10:10:40',
                      text: 'La Marianna va in campagna',
-                     status: 'received'
+                     status: 'received',
+                     isBoxClick: false,
                    },
                    {
                      date: '28/03/2020 10:20:10',
                      text: 'Sicuro di non aver sbagliato chat?',
-                     status: 'sent'
+                     status: 'sent',
+                     isBoxClick: false,
                    },
                    {
                      date: '28/03/2020 16:15:22',
                      text: 'Ah scusa!',
-                     status: 'received'
+                     status: 'received',
+                     isBoxClick: false,
                    }
                    ],
                  },
-                   {               
+                   {
+                    id: 4,               
                    name: 'Luisa Amet',
                    avatar: '_6',
                    visible: true,
                    messages: [{
                      date: '10/01/2020 15:30:55',
                      text: 'Lo sai che ha aperto una nuova pizzeria?',
-                     status: 'sent'
+                     status: 'sent',
+                     isBoxClick: false,
                    },
                    {
                      date: '10/01/2020 15:50:00',
                      text: 'Si, ma preferirei andare al cinema',
-                     status: 'received'
+                     status: 'received',
+                     isBoxClick: false,
                    }
                    ],
                  },
@@ -98,6 +113,7 @@ const app = Vue.createApp({
             newMessage: '',
             searchContact : '',  
             lastIndex : 0,
+            
             }
         },
         computed:{
@@ -134,7 +150,17 @@ const app = Vue.createApp({
          },
          getLastIndex(index){
             return this.contacts[index].messages.length -1;
-         }
+         },
+        showDeleteMenu(index){
+          this.contacts[this.selectedIndex].messages[index].isBoxClick= !this.contacts[this.selectedIndex].messages[index].isBoxClick;
+
+        },
+        deleteMessage(index){
+          if(this.contacts[this.selectedIndex].messages){
+           this.contacts[this.selectedIndex].messages.splice(index,1);
+           this.showDeleteMenu(index);
+          }
+        }
         }
 
     })
